af63eef32f2d761d3a112d127ac706b1f6f8529f
Marius Karlsen <marius.r.karlsen@hiof.no>
2018-10-04 11:44:34 +0200

    added error handling

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,12 +10,18 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 	glShaderSource(id, 1, &src, nullptr);
 	glCompileShader(id);
 
-	// TODO: Error handling
+	/* Error handling */
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+
+	}
 
 	return id;
 }
 
-static int CreateShader(const std::string& vertexShader, const std::string &fragmentShader)
+static unsigned int CreateShader(const std::string& vertexShader, const std::string &fragmentShader)
 {
 	unsigned int program = glCreateProgram();
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
@@ -28,6 +34,8 @@ static int CreateShader(const std::string& vertexShader, const std::string &frag
 
 	glDeleteShader(vs);
 	glDeleteShader(fs);
+
+	return program;
 }
 
 int main(void)
@@ -54,22 +62,22 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	// Vertexpositions
+	/* Vertexpositions */
 	float positions[6] = {
 		-0.5f, -0.5f,
 		 0.0f,  0.5f,
 		 0.5f, -0.5f,
 	};
 
-	// Creating and binding a vertexbuffer
+	/* Creating and binding a vertexbuffer*/
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
-	// Enabeling vertex attribute array
+	/* Enabeling vertex attribute array */
 	glEnableVertexAttribArray(0);
-	// Creating vertex attribute array
+	/* Creating vertex attribute array */
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
